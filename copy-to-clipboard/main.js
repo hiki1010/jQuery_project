5861053fd9304623d941825008f4958e111329b7
hiki1010 <nicole.gabe@yandex.ru>
2022-05-26 16:14:10 +1000

    26052022

---
+++ copy-to-clipboard/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    function add() {
+      $(".copied").addClass("bounce-effect");
+    }
+    function remove() {
+      $(".copied").removeClass("bounce-effect");
+    }
+    $(".copy-btn").click(function () {
+      $("#textField").select();
+      document.execCommand("copy");
+      add();
+      setTimeout(remove, 800);
+    });
+  });
+  
